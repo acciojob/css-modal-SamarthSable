0e34e3770593cfd69ccf18b3f7df0c4f34640a1a
SamarthSable <143312697+SamarthSable@users.noreply.github.com>
2025-11-29 19:17:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.querySelector("#openModal").onclick = () => {
+    document.querySelector(".modal").style.display = "block";
+};
+
+document.querySelector(".close-modal").onclick = () => {
+    document.querySelector(".modal").style.display = "none";
+};
