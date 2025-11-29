c9ea4cb0e2f33932de3b7015f58a29e0f45cc3e7
SamarthSable <143312697+SamarthSable@users.noreply.github.com>
2025-11-29 19:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//your JS code here. If required.
 document.querySelector("#openModal").onclick = () => {
-    document.querySelector(".modal").style.display = "block";
+    document.querySelector(".modal").style.display = "flex";
 };
 
 document.querySelector(".close-modal").onclick = () => {
